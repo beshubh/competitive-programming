8e0a5895c2479093124d0d7f9ee9aa61ed476038
shubhamsks <shubhamkumar7051@gmail.com>
2019-03-01 08:00:10 +0530

    sorting_algos include

---
+++ sortingAlgos/merge_sorted.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+void merge(int *arr, int s, int mid, int e){
+    int i = s;
+    int j = mid + 1;
+    int k = 0;
+    int * temp = new int[e - s + 1];
+    while( i <= mid && j <= e){
+        if(arr[i] < arr[j]){
+            temp[k++] = arr[i++];
+        }
+        else
+        {
+            temp[k++] = arr[j++];
+        }
+    }
+    while( i <= mid){
+        temp[k++] = arr[i++];
+    }
+    while( j <= e){
+        temp[k++] = arr[j++];
+    }
+    for( i=s,k = 0; i <= e; i++, k++)
+    {
+        arr[i] = temp[k];
+    }
+}
+void merge_sort(int *arr,int s,int e){
+    if(s>=e){
+        return;
+    }
+    int mid = (s + e)/2;
+    merge_sort(arr, s, mid);
+    merge_sort(arr, mid + 1,e);
+    merge(arr, s, mid, e);
+}
+int main(){
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    merge_sort(arr,0,n-1);
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
